a955f4b48f5b3cf83a33c4c071f38b1ed3f8297a
Antony Quang <antony.quang@googlemail.com>
2022-06-22 00:04:06 +0100

    first commit

--- methodology_reader.py
+++ methodology_reader.py
@@ -1,11 +1,29 @@
 import os
+from pprint import pprint
+
+
 txt_quantity = 0
-dir = "methodology text"
-for path in os.listdir(dir):
-    if os.path.isfile(os.path.join(dir, path)):
+methodology_text = []
+text_dir = "methodology text"
+
+for path in os.listdir(text_dir):
+    if os.path.isfile(os.path.join(text_dir, path)):
         txt_quantity += 1
-print(txt_quantity)
 
 for i in range(1, txt_quantity+1):
-    with open(f"{dir}/part_{i}.txt", mode='r', encoding='utf8') as file:
+    with open(f"{text_dir}/part_{i}.txt", mode='r', encoding='utf8') as file:
         text = file.read()
+        methodology_text.append(text)
+
+
+equation_quantity = 0
+methodology_equations = []
+equation_dir = "methodology equations"
+for path in os.listdir(equation_dir):
+    if os.path.isfile(os.path.join(equation_dir, path)):
+        equation_quantity += 1
+
+for i in range(1, equation_quantity+1):
+    equation_file_name = f"{equation_dir}/equation {i}.png"
+    methodology_equations.append(equation_file_name)
+
